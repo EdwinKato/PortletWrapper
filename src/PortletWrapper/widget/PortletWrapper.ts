e0cf78a19ca306705cf853efe3d5230300d2fedb
Edwin Kato <katedwin88@gmail.com>
2017-01-25 11:31:16 +0300

    Pass properties from xml to Component

--- src/PortletWrapper/widget/PortletWrapper.ts
+++ src/PortletWrapper/widget/PortletWrapper.ts
@@ -1,47 +1,95 @@
 import * as dojoDeclare from "dojo/_base/declare";
 import * as WidgetBase from "mxui/widget/_WidgetBase";
 import { createElement } from "react";
-import { render } from "react-dom";
+import { render, unmountComponentAtNode } from "react-dom";
 
-import { PortletWrapper as PortletWrapperComponent  } from "./components/PortletWrapper";
+import { PortletWrapper as PortletWrapperComponent, PortletWrapperProps  } from "./components/PortletWrapper";
 
 
 class PortletWrapper extends WidgetBase {
 
+    // Parameters configured from modeler
+    private objectType: string;
+    private password: string;
+    private username: string;
+    private serverURL: string;
+    private height: number;
+    private width: number;
+
+    // Internal variables
     private contextObject: mendix.lib.MxObject;
-    private handles: any[];
-    private value: number;
 
     postCreate() {
-        this.handles = [];
         this.updateRendering();
     }
 
-    update(object: mendix.lib.MxObject, callback?: Function) {
+    update(object: mendix.lib.MxObject, callback: Function) {
         this.contextObject = object;
-        this.updateRendering(callback);
-        this._resetSubscriptions();
+        this.resetSubscriptions();
+        this.updateRendering();
+
+        callback();
     }
 
-    private updateRendering(callback?: Function) {
-        // render your component here
+    uninitialize(): boolean {
+        unmountComponentAtNode(this.domNode);
+
+        return true;
     }
-    private _unsubscribe() {
-        if (this.handles) {
-            for (let handle of this.handles) {
-                mx.data.unsubscribe(handle);
-            }
-            this.handles = [];
-        }
+
+    private updateRendering() {
+        render(createElement(PortletWrapperComponent, this.getPortletWrapperProps()), this.domNode);
     }
-    private _resetSubscriptions() {
-        this._unsubscribe();
+
+    private getPortletWrapperProps(): PortletWrapperProps {
+        const serverURL = this.contextObject && this.serverURL
+            ? (this.contextObject.get(this.serverURL))as string
+            : "";
+        const username = this.contextObject && this.username
+            ? (this.contextObject.get(this.username))as string
+            : "";
+        const password = this.contextObject && this.password
+            ? (this.contextObject.get(this.password))as string
+            : "";
+
+        return {
+            height: this.height,
+            width: this.width,
+            serverURL,
+            username,
+            password
+        };
+    }
+
+    private resetSubscriptions() {
+        this.unsubscribeAll();
         if (this.contextObject) {
-            // Subscriptions
+            this.subscribe({
+                callback: (guid) => this.updateRendering(),
+                guid: this.contextObject.getGuid()
+            });
+            this.subscribe({
+                attr: this.serverURL,
+                callback: (guid, attr, attrValue) => this.updateRendering(),
+                guid: this.contextObject.getGuid()
+            });
+            this.subscribe({
+                attr: this.username,
+                callback: (guid, attr, attrValue) => this.updateRendering(),
+                guid: this.contextObject.getGuid()
+            });
+            this.subscribe({
+                attr: this.password,
+                callback: (guid, attr, attrValue) => this.updateRendering(),
+                guid: this.contextObject.getGuid()
+            });
         }
     }
 }
 
+// Declare widget prototype the Dojo way
+// Thanks to https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/dojo/README.md
+// tslint:disable : only-arrow-functions
 let dojoPortletWrapper = dojoDeclare(
     "PortletWrapper.widget.PortletWrapper",
     [ WidgetBase ],
